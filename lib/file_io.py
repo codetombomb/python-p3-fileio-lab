1b017e4020b43d82edba1c555b6fe6e6971c7baa
Tom Tobar <cocdetombomb@gmail.com>
2022-12-23 21:38:47 -0600

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    new_file = open(file_name + ".txt", encoding="utf-8", mode="w")
+    new_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    new_file = open(file_name + ".txt", encoding="utf-8", mode="a")
+    new_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    my_file = open(file_name + ".txt", encoding="utf-8")
+    return my_file.read()
